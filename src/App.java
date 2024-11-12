bfeb4c8657f85debceb12d5fd1897378e3811fde
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-11-12 10:23:59 +0100

    baodja

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner namn = new Scanner(System.in); 
 
-        if (6 >= 3) {
+        System.out.println("Välj ett användernamn: ");
+        String användernamn = namn.nextLine();
+           if(användernamn== "kalleanka"){
+            System.out.println("welcome");
+           }
+       // if (6 >= 3) {
 
-            System.out.println("Hello, World!");
+           // System.out.println("Hello, World!")
+
         }
     }
-}
+//}
