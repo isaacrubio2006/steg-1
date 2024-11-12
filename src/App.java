59aa08be226a11e2b71e3d6ca024f0ed8167bec9
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-11-12 11:08:03 +0100

    hqw

--- src/App.java
+++ src/App.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner namn = new Scanner(System.in); 
+        Scanner namn = new Scanner(System.in);
 
-        System.out.println("Välj ett användernamn: ");
-        String användernamn = namn.nextLine();
-           if(användernamn== "kalleanka"){
-            System.out.println("welcome");
-           }
-       // if (6 >= 3) {
-
-           // System.out.println("Hello, World!")
+        String Password = namn.nextLine();
+        while (!Password.equals("12345")) {
+            System.out.println("Sriv ditt lösenord: ");
+            Password = namn.nextLine();
 
         }
+
+        // int Tal= 0;
+        // while (Tal < 32) {
+        // System.out.println("hello, world");
+        // Tal++;
+
+        // }
+
+        // System.out.println("Skriv ditt användernamn: ");
+        // String användernamn = namn.nextLine();
+        // System.out.println("Skriv ditt lösenord: ");
+        // String Lösen = namn.nextLine();
+
+        // if (användernamn.equals("kalleanka") && Lösen.equals("12345")) {
+        // System.out.println("welcome");
+        // }
+        // if (6 >= 3) {
+
+        // System.out.println("Hello, World!")
+
     }
-//}
+}
+// }
