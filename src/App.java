69c8f1d0812dcf3fc47d6baa65ed2e8470c7ad95
isaac rubio <isaacamaru.rubio@elev.ga.ntig.se>
2024-11-12 10:16:00 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int = 5; 
+        if (Tal==6){
+
+        }
+        System.out.println("Hello, World!");
+    }
+}
